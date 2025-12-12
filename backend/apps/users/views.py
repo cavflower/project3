1c359cea6c5a09a32f8bea9f28caca6b83a1f269
cavflower <cavflower@gmail.com>
2025-12-12 23:47:13 +0800

    增加錯誤訊息

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -21,7 +21,7 @@ class FirebaseTokenLoginView(APIView):
 
         try:
             # Verify the ID token with Firebase Admin SDK
-            decoded_token = firebase_auth.verify_id_token(id_token)
+            decoded_token = firebase_auth.verify_id_token(id_token, check_revoked=False, clock_skew_seconds=5)
             uid = decoded_token['uid']
 
             # Get or create the user in your Django database
